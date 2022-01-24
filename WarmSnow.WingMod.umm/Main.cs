52abb4f2ceba2f84894436485a43cf19be4dd530
WingGao <wing.gao@live.com>
2022-01-24 19:20:02 +0800

    feat: 修改掉落

--- WarmSnow.WingMod.umm/Main.cs
+++ WarmSnow.WingMod.umm/Main.cs
@@ -18,6 +18,7 @@ namespace WingMod
             [Header("修改")] [Draw("掉落神兵/圣物/技能最高等级")]
             public bool DropWeaponLevel3 = true;
 
+            [Draw("见闻掉落无限制")] public bool DropStoryUnlimited = true;
             [Draw("剑返自动触发")] public bool FlySwardAutoBack = true;
             [Draw("剑返无冷却")] public bool FlySwardBackNoCd = true;
 
@@ -33,6 +34,10 @@ namespace WingMod
             [Draw("伤害倍率", Min = 1, Precision = 0)] public float DamageMultiply = 1f;
 
             [Draw("魂不减")] public bool SoulNotDecrease = true;
+
+            public Settings()
+            {
+            }
 
             public void OnChange()
             {
@@ -51,7 +56,7 @@ namespace WingMod
 
         static void Load(UnityModManager.ModEntry modEntry)
         {
-            settings = Settings.Load<Settings>(modEntry);
+            settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
 
             Harmony.DEBUG = true;
             FileLog.Reset();
@@ -64,6 +69,8 @@ namespace WingMod
             modEntry.OnToggle = OnToggle;
             modEntry.OnGUI = OnGUI;
             modEntry.OnSaveGUI = OnSaveGUI;
+
+            EnemyControlPatch.Init();
 
             LogF("WingMod load");
         }
@@ -108,14 +115,14 @@ namespace WingMod
         public static class PotionDropPool_Pop
         {
             [HarmonyPrefix]
-            [HarmonyPatch("Pop", new[] {typeof(PN), typeof(int), typeof(Vector3)})]
+            [HarmonyPatch("Pop", new[] { typeof(PN), typeof(int), typeof(Vector3) })]
             public static void Prefix1(ref int level)
             {
                 if (settings.DropWeaponLevel3) level = 2; //金色
             }
 
             [HarmonyPrefix]
-            [HarmonyPatch("Pop", new[] {typeof(int), typeof(int), typeof(Vector3)})]
+            [HarmonyPatch("Pop", new[] { typeof(int), typeof(int), typeof(Vector3) })]
             static void Prefix2(ref int level)
             {
                 Prefix1(ref level);
@@ -190,14 +197,26 @@ namespace WingMod
         static bool IsFlySwardBackCdOk()
         {
             return PlayerAnimControl.instance.DrawCoolDownTimer >= PlayerAnimControl.instance.drawCoolDown *
-                (1.0 - (double) PlayerAnimControl.instance.playerParameter.DRAW_SWORD_CD_REDUCE);
+                (1.0 - (double)PlayerAnimControl.instance.playerParameter.DRAW_SWORD_CD_REDUCE);
         }
 
         // 怪物受伤
-        [HarmonyPatch(typeof(EnemyControl), "DealDamage")]
-        public static class EnemyControl_DealDamage
+        [HarmonyPatch(typeof(EnemyControl))]
+        public static class EnemyControlPatch
         {
-            static void Prefix(EnemyControl __instance, ref float damage)
+            private static HashSet<int> StoryIds = new HashSet<int>();
+
+            public static void Init()
+            {
+                for (var i = 0; i <= 34; i++)
+                {
+                    StoryIds.Add(i);
+                }
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("DealDamage")]
+            static void DealDamagePrefix(EnemyControl __instance, ref float damage)
             {
                 //自动剑返
                 if (settings.FlySwardAutoBack && PlayerAnimControl.instance.playerParameter.SWORDS_COUNT == 0 &&
@@ -213,6 +232,37 @@ namespace WingMod
                     PlayerAnimControl.instance.DrawCoolDownTimer = PlayerAnimControl.instance.drawCoolDown;
                 // 伤害倍率
                 damage *= settings.DamageMultiply;
+            }
+
+            // 修改过的
+            private static HashSet<EnemyControl> patchedMonster = new HashSet<EnemyControl>();
+
+            // 掉落
+            [HarmonyPrefix]
+            [HarmonyPatch("Drop")]
+            static void DropPrefix(EnemyControl __instance)
+            {
+                if (settings.DropStoryUnlimited && __instance.storyChipDropProb != null)
+                {
+                    if (!patchedMonster.Contains(__instance))
+                    {
+                        patchedMonster.Add(__instance);
+                        var dropSet = new HashSet<int>(StoryIds);
+                        __instance.storyChipDrop.ForEach(t => dropSet.Remove(t));
+                        foreach (var i in dropSet) //添加剩余的掉落
+                        {
+                            __instance.storyChipDrop.Add(i);
+                            __instance.storyChipDropProb.Add(0.5f);
+                        }
+                    }
+                }
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("OnDisable")]
+            static void OnDisablePrefix(EnemyControl __instance)
+            {
+                patchedMonster.Remove(__instance); //移除patch标记
             }
         }
     }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Wing的Unity相关Mod
+
+
+## 开发相关
+* 创建一个mod工程
+* 将游戏`Managed`映射为`_game`, 目录连接
+* 
