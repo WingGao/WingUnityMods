a153f60899a62cafd1cb8eeccff03c5ae906db0c
Wing Gao <wing.gao@live.com>
2022-08-19 00:36:06 +0800

    feat: 添加功能

--- BannerOfTheMaid.bie5/PluginMain.cs
+++ BannerOfTheMaid.bie5/PluginMain.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using France.Game.controller;
 using France.Game.model.level;
 using France.Game.model.role;
+using France.Resource;
 using FranceGame.Plugins;
 using Game.Client;
 using HarmonyLib;
@@ -16,7 +18,7 @@ namespace WingMod
     {
         public static Harmony harmony = new Harmony("WingMod");
         public static Plugin Instance;
-        
+
 
         private void Awake()
         {
@@ -39,14 +41,14 @@ namespace WingMod
             [HarmonyPatch(typeof(Debugger), "Log", new Type[] {typeof(LogLevel), typeof(object)})]
             static void Prefix1(object message)
             {
-                Instance.Logger.LogInfo("[GameLog]"+message);
+                Instance.Logger.LogInfo("[GameLog]" + message);
             }
 
             [HarmonyPrefix]
             [HarmonyPatch(typeof(Debugger), "Log", new Type[] {typeof(LogLevel), typeof(string), typeof(object[])})]
             static void Prefix2(string format, params object[] args)
             {
-                Instance.Logger.LogInfo(string.Format("[GameLog]"+format, args));
+                Instance.Logger.LogInfo(string.Format("[GameLog]" + format, args));
             }
         }
         // static class UIComponentPatcher
@@ -58,25 +60,91 @@ namespace WingMod
         //         Instance.Logger.LogInfo($"Open {__instance.uiName}");
         //     }
         // }
-        
+
         /// France.Game.controller.BattleSceneController.currentOPerActor 战斗元素
         /// 
         /// 
-
         static class RolePatcher
         {
             [HarmonyPostfix]
             [HarmonyPatch(typeof(Role), MethodType.Constructor, new Type[] {typeof(int)})]
-            static void InitPost(Role __instance,int characterId)
+            static void InitPost(Role __instance, int characterId)
             {
                 Instance.Logger.LogInfo($"Role.Init {characterId}");
             }
-            
+
+            // 升级的时候全属性增加
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Role), "lvUp")]
+            static void Role_lvUp(Role __instance)
+            {
+                CharacterRoleData basicInfo = __instance.getBasicInfo();
+                CharacterClassData classInfo = __instance.getClassInfo();
+                var toLv = __instance.lv + 1;
+
+                LvUpPropertyDataCollection propertyDataCollection = !basicInfo.lvupData.ContainsKey(__instance.classId)
+                    ? basicInfo.lvupData.First<KeyValuePair<int, LvUpPropertyDataCollection>>().Value
+                    : basicInfo.lvupData[__instance.classId];
+                Instance.Logger.LogInfo($"Role_lvUp {basicInfo.name} lv={toLv} {propertyDataCollection}");
+                if (propertyDataCollection.datas.ContainsKey(toLv))
+                {
+                    CharacterLvUpPropertyData data = propertyDataCollection.datas[toLv];
+                    data.growthHP = Math.Max(data.growthHP, 1);
+                    data.growthATT = Math.Max(1, data.growthATT);
+                    data.growthDEF[0] = Math.Max(1, data.growthDEF[0]);
+                    data.growthDEF[1] = Math.Max(1, data.growthDEF[1]);
+                    data.growthDEX = Math.Max(1, data.growthDEX);
+                    data.growthSPD = Math.Max(1, data.growthSPD);
+                    data.growthLUCK = Math.Max(1, data.growthLUCK);
+                }
+            }
+
+            // 最少获得经验
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Role), "gainExp")]
+            static void Role_gainExp(ref int exp)
+            {
+                if (exp <= 0)
+                {
+                    exp = 3;
+                }
+            }
+
             [HarmonyPrefix]
             [HarmonyPatch(typeof(BattleSceneController), "attackTarget")]
-            static void BattleSceneController_attackTarget_Post(BattleSceneController __instance,ref GFightUnit attacker)
+            static void BattleSceneController_attackTarget_Post(BattleSceneController __instance, ref GFightUnit attacker)
             {
-                Instance.Logger.LogInfo($"attack {attacker.getRole().getBasicInfo().name}  hp={attacker.getHp()}");
+                var role = attacker.getRole();
+                var weapon = attacker.getWeapon();
+                // 自己人武器耐久全满
+                if (attacker.getTeamId() == 0)
+                {
+                    weapon.count = weapon.getItemData().maxCount;
+                }
+                Instance.Logger.LogInfo($"attack {role.getBasicInfo().name}({role.characterId}) teamId={attacker.getTeamId()}  hp={attacker.getHp()} " +
+                                        $"weaponCount={weapon.count}");
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(GFightUnit), "setHp")]
+            static void GFightUnit_setHp_pre(GFightUnit __instance, ref float value)
+            {
+                if (__instance.getTeamId() == 0)
+                {
+                    //自己人
+                    if (value <= 1)
+                    {
+                        value = 1;
+                    }
+                }
+            }
+
+            // 对话框无延迟
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(PopDialogManager), "setDelay")]
+            static void PopDialogManager_setDelay(ref int delayFrame)
+            {
+                delayFrame = 0;
             }
         }
     }
