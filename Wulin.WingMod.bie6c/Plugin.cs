d29955ac5de2b31086bcd7ae6091d203ded7a1ea
WingGao <wing.gao@live.com>
2023-05-25 21:45:37 +0800

    feat: wulin

---
+++ Wulin.WingMod.bie6c/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace Wulin.WingMod.bie6c
+{
+    [BepInPlugin("WingMod", "WingMod", "0.0.1")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin WingMod is loaded!");
+        }
+    }
+}
