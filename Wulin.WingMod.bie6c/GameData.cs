246aa3bbd314f959ddff18ed6b1b9c4bddfa9935
WingGao <wing.gao@live.com>
2023-05-26 18:57:55 +0800

    feat: 修改

---
+++ Wulin.WingMod.bie6c/GameData.cs
@@ -0,0 +1,21 @@
+﻿namespace Wulin.WingMod.bie6c;
+
+public class GameData
+{
+}
+
+public static class CharacterPropKey
+{
+    public const string 武功使用经验 = "武功使用经验";
+    public const string 实战经验获得 = "实战经验获得";
+    public const string 属性提升加成_ = "属性提升加成_";
+    public const string 点穴 = "点穴";
+    public const string 视野失效 = "视野失效";
+    public const string 怒气上限 = "怒气上限";
+    public const string 男性好感提升加成 = "男性好感提升加成";
+    public const string 非宠物好感提升加成 = "非宠物好感提升加成";
+    public const string 宠物好感提升加成 = "宠物好感提升加成";
+    public const string 宠物饱食度 = "宠物饱食度";
+    //AdditionProp
+    public const string 礼节 = "礼节";
+}
--- Wulin.WingMod.bie6c/Plugin.cs
+++ Wulin.WingMod.bie6c/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Il2CppSystem;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.IL2CPP;
@@ -14,11 +15,17 @@ namespace Wulin.WingMod.bie6c
         public static Plugin Instance;
 
         public ConfigEntry<float> KongfuExpMultiplier;
+        public ConfigEntry<float> NengliExpMultiplier;
+        public ConfigEntry<float> HaoGanExpMultiplier;
+        public ConfigEntry<bool> DaodeFlag;
 
         public override void Load()
         {
             Instance = this;
-            KongfuExpMultiplier = Config.Bind("修改", "KongfuExpMultiplier", 10f, new ConfigDescription("武功经验倍率"));
+            KongfuExpMultiplier = Config.Bind("修改", "武功经验倍率", 10f);
+            NengliExpMultiplier = Config.Bind("修改", "能力经验倍率", 10f);
+            HaoGanExpMultiplier = Config.Bind("修改", "好感倍率", 10f);
+            DaodeFlag = Config.Bind("修改", "道德不减", true);
             // Harmony.CreateAndPatchAll(typeof(Plugin));
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
             HarmonyFileLog.Enabled = true;
@@ -35,16 +42,101 @@ namespace Wulin.WingMod.bie6c
         [HarmonyPatch]
         static class MyPatcher
         {
-            [HarmonyPatch(typeof(KungfuInstance))]
-            static class KungfuInstancePatch
+            [HarmonyPatch(typeof(GameCharacterInstance))]
+            static class GameCharacterInstance_Patch
             {
-                [HarmonyPatch(nameof(KungfuInstance.AddExp))]
-                [HarmonyPrefix]
-                static void AddExp_Patch(ref int amount)
+                /// <summary>
+                /// 属性计算，像经验倍率什么的
+                /// </summary>
+                /// <param name="key"></param>
+                /// <param name="origin"></param>
+                [HarmonyPatch(nameof(GameCharacterInstance.ModifyValueByPropAsBuff),
+                    new[] { typeof(string), typeof(int), typeof(GameCharacterInstance.FinalPropSource) })]
+                [HarmonyPostfix]
+                static void ModifyValueByPropAsBuff_Patch(string key, int origin, ref int __result)
                 {
-                    amount = (int)(amount * Instance.KongfuExpMultiplier.Value);
-                    LogInfo($"AddExp_Patch {amount} {new System.Diagnostics.StackTrace()}");
+                    Decimal r = new Decimal(__result);
+                    ModifyValueByPropAsBuff2_Patch(key, new Decimal(origin), ref r);
+                    __result = (int)r;
                 }
+
+                [HarmonyPatch(nameof(GameCharacterInstance.ModifyValueByPropAsBuff),
+                    new[] { typeof(string), typeof(Decimal), typeof(GameCharacterInstance.FinalPropSource) })]
+                [HarmonyPostfix]
+                static void ModifyValueByPropAsBuff2_Patch(string key, Decimal origin, ref Decimal __result)
+                {
+                    if (__result <= 0) return; //排除减小的情况
+
+                    var needLog = true;
+                    switch (key)
+                    {
+                        case CharacterPropKey.武功使用经验:
+                        case CharacterPropKey.实战经验获得:
+                            __result = origin * (Decimal)Instance.KongfuExpMultiplier.Value;
+                            break;
+                        case CharacterPropKey.非宠物好感提升加成:
+                        case CharacterPropKey.宠物好感提升加成:
+                            __result = origin * (Decimal)Instance.HaoGanExpMultiplier.Value;
+                            break;
+                        case CharacterPropKey.点穴:
+                        case CharacterPropKey.视野失效:
+                            needLog = false;
+                            break;
+                        default:
+                            if (key.StartsWith("属性提升加成_"))
+                            {
+                                var nk = key.Substring("属性提升加成_".Length);
+                                if (nk.StartsWith("能力经验_") || nk.EndsWith("经验") || nk == CharacterPropKey.宠物饱食度)
+                                {
+                                    // 属性提升加成_敏捷经验
+                                    // 属性提升加成_名声经验
+                                    // 属性提升加成_能力经验_生存_识图
+                                    __result = origin * (Decimal)Instance.NengliExpMultiplier.Value;
+                                }
+                            }
+
+                            break;
+                    }
+
+
+                    if (needLog)
+                        LogInfo($"ModifyValueByPropAsBuff_Patch key={key} origin={origin} __result={__result}");
+                }
+
+                [HarmonyPatch(nameof(GameCharacterInstance.ChangeOriginProp))]
+                [HarmonyPostfix]
+                static void ChangeOriginProp_Patch(string key, Decimal value)
+                {
+                    LogInfo($"ChangeOriginProp_Patch key={key} value={value}");
+                }
+
+                [HarmonyPatch(nameof(GameCharacterInstance.ChangeAdditionProp))]
+                [HarmonyPrefix]
+                static void ChangeAdditionProp_Patch(string key, ref Decimal value)
+                {
+                    if (Instance.DaodeFlag.Value && key == CharacterPropKey.礼节)
+                    {
+                        if (value < 0) value *= -1;
+                    }
+
+                    LogInfo($"ChangeAdditionProp_Patch key={key} value={value}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(KungfuInstance))]
+        static class KungfuInstancePatch
+        {
+            /// <summary>
+            /// 武功使用经验
+            /// </summary>
+            /// <param name="amount"></param>
+            // [HarmonyPatch(nameof(KungfuInstance.AddExp))]
+            // [HarmonyPrefix]
+            static void AddExp_Patch(ref int amount)
+            {
+                amount = (int)(amount * Instance.KongfuExpMultiplier.Value);
+                LogInfo($"AddExp_Patch {amount} {new System.Diagnostics.StackTrace()}");
             }
         }
     }
